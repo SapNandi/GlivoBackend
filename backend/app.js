138d4298d7a73611ba5b945c61a6808435a789a1
Saptarshi Nandi <saptarshinandi2001@gmail.com>
2023-11-01 05:11:36 +0530

    commit5

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const errorMiddleware = require("./Middleware/error");
 // Middleware
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(
-  cors({ origin: "https://glivo-frontend.vercel.app/", credentials: true })
+  cors({ origin: "glivo-frontend.vercel.app/", credentials: true })
 );
 app.use(express.json());
 app.use(cookieParser());
